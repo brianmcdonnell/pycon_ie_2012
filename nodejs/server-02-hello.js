547f5683f5b2010d7a430b9f5197aad4f09ed000
Brian McDonnell <mcdonnellb@gmail.com>
2012-10-09 23:05:40 +0100

    Minor tidy

--- nodejs/server-02-hello.js
+++ nodejs/server-02-hello.js
@@ -2,11 +2,13 @@ var http = require("http");
 var url = require('url');
 
 http.createServer(function(request, response) {
+
     setTimeout(function(){
         var parts = url.parse(request.url, true);
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write("Hello World " + parts.query.user + " (delayed)");
         response.end();
     }, 1000);
+
 }).listen(8080);
 
