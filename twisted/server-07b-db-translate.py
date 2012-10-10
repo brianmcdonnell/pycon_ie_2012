c6988de762f08505cbbab0baae53c7d615470c7c
Brian McDonnell <mcdonnellb@gmail.com>
2012-10-11 00:53:46 +0100

    Nested inlinecallback with return value example.

---
+++ twisted/server-07b-db-translate.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+from twisted.web import server, resource
+from twisted.internet import defer, reactor
+from twisted.web.server import NOT_DONE_YET
+
+from utils import validate_params
+import txmongo
+
+SVC_HOST = 'localhost'
+SVC_PORT = 8010
+
+
+class TranslatorResource(resource.Resource):
+    isLeaf = True
+
+    def render_GET(self, request):
+        error_str = validate_params(request, ('user', 'data'))
+        if error_str: return error_str
+
+        # 
+        self.handle_request(request)
+
+        return NOT_DONE_YET
+
+    @defer.inlineCallbacks
+    def handle_request(self, request):
+        try:
+            username = request.args['user'][0]
+            user = yield self.get_user(username)
+            if user:
+                # Call the translator service
+                input_str = request.args['data'][0]
+                from txtranslator import TranslatorClient
+                client = TranslatorClient(SVC_HOST, SVC_PORT)
+                output_str = yield client.translate2(input_str)
+                request.write("%s: %s" % (str(user['name']), output_str))
+            else:
+                request.write("User '%s' not found!" % username)
+        except Exception, err:
+            request.write(resource.ErrorPage(500, "Internal Server Error.", err).render(request))
+        finally:
+            request.finish()
+
+    @defer.inlineCallbacks
+    def get_user(self, username):
+        cxn = yield txmongo.MongoConnection()
+        user = yield cxn.pycon.users.find_one({'name': username})
+        # Necessary to return a value from an inlineCallback
+        defer.returnValue(user)
+
+root = resource.Resource()
+root.putChild("translate", TranslatorResource())
+site = server.Site(root)
+reactor.listenTCP(8080, site)
+reactor.run()
