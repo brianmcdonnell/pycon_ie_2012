afbcd7a8037e62d751363dfdddab64dacba87e5c
Brian McDonnell <mcdonnellb@gmail.com>
2012-11-08 15:33:44 +0000

    Simplified implementation with usable socket.

--- pysockets/server-st-blocking.py
+++ pysockets/server-st-blocking.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 import socket
-import sys
+sck_lstn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sck_lstn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sck_lstn.bind(('', 8888))
+sck_lstn.listen(10)
 
-HOST = ''
-PORT = 8888
-BACKLOG_SIZE = 10
+def handle_request(sck_new):
+    str_buf = ''
+    end_of_request = False
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(BACKLOG_SIZE)
+    while not end_of_request:
+        str_buf += sck_new.recv(8) # BLOCKING
+        if str_buf.strip().endswith('.'):
+            end_of_request = True
+    sck_new.send(str_buf.upper()) # BLOCKING
 
-print "Listening"
-
-conn, addr = s.accept()
-print 'Connected with ' + addr[0] + ':' + str(addr[1])
+while True:
+    sck_new, addr = sck_lstn.accept() # BLOCKING
+    handle_request(sck_new)
